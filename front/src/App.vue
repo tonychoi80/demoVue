ff4c096bcde12848e94aac420575f0ec7640df43
tony <yb.choi@myatman.com>
2018-11-15 16:37:09 +0900

    no message

--- front/src/App.vue
+++ front/src/App.vue
@@ -12,7 +12,10 @@ export default {
     titleTemplate: '%s - Yay!', // title is now "My Example App - Yay!"
     htmlAttrs: {
       amp: undefined // "amp" has no value
-    }
+    },
+    meta: [
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ]
   }
 }
 </script>
